99f422fbb0fa65efe51758903d9af1cbbf0112d4
yassineraddaoui <yassineraddaouicr7@gmail.com>
2022-07-14 19:25:56 +0200

    v2

--- src/main/java/com/datajpa/springdatajpa/repo/StudentRepository.java
+++ src/main/java/com/datajpa/springdatajpa/repo/StudentRepository.java
@@ -1,9 +1,16 @@
 package com.datajpa.springdatajpa.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.datajpa.springdatajpa.entitie.Student;
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-}
+	List<Student> findByFirstName(String firstName);
+	List <Student> findByFirstNameContaining(String name);
+	List<Student> findByLastNameNotNull();
+	List <Student> findByGuardianName(String name);
+	List <Student> findByFirstNameContainingIgnoreCase(String name);
+	}
--- src/test/java/com/datajpa/springdatajpa/repo/StudentRepositoryTest.java
+++ src/test/java/com/datajpa/springdatajpa/repo/StudentRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.datajpa.springdatajpa.repo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,7 +35,7 @@ class StudentRepositoryTest {
 		Student student=Student.builder()
 				.emailId("cdsc@d.c")
 				.lastName("qdsdqs")
-				.firstName("sqdsq")
+				.firstName("yassin")
 				.guardian(guardian)
 				.build();
 		
@@ -45,5 +46,30 @@ class StudentRepositoryTest {
 		List<Student> st= studentRepository.findAll();
 		System.out.println(st);
 	}
-	
+	@Test
+	public void findByFirstNameContainingIgnoreCase() {
+		List<Student> st= studentRepository.findByFirstNameContainingIgnoreCase("yAS");
+		System.out.println("findByFirstNameContainingIgnoreCase\n"+st);
+	}
+//	@Test
+//	public void findbyFirstName() {
+//		List<Student> st= studentRepository.findByFirstName("yassin");
+//		System.out.println("findbyFirstName\n"+st);
+//	}
+	@Test
+	public void findbyFirstNameContaining() {
+		List<Student> stc= studentRepository.findByFirstNameContaining("yAs");
+		System.out.println("findbyFirstNameContaining\n"+stc);
+	}
+//	@Test
+//	public void findByLastNameNotNull() {
+//		List<Student> stc= studentRepository.findByLastNameNotNull();
+//		System.out.println("findByLastNameNotNull\n"+stc);
+//	}
+//	@Test
+//	public void findByGuardianName() {
+//		List<Student> stc= studentRepository.findByGuardianName("yassin");
+//		System.out.println("findByGuardianName \n"+stc);
+//	}
+//	
 }
